abe9d463a7d3ca661e3e3f8cc58c6376dc74345c
DarkenLM <rafaelsantosfernandes660@gmail.com>
2025-01-12 00:29:03 +0000

    chore(code): added initial Blazor Web App implementation
    - Added an initial skeleton for the project based on the Blazor Web App built-in template.
    - NOTE: Testing required, specially for the SignalR Hub implementation.

---
+++ LI4/LI4/Hubs/ChatHub.cs
@@ -0,0 +1,10 @@
+﻿namespace LI4.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+public class ChatHub : Hub {
+    public static readonly string URL = "/api/ws/chat";
+
+    public async Task SendMessage(string user, string message) {
+        await Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
