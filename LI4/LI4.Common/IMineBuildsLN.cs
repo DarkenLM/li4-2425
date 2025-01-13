93724d15feb11f0b19a87b0db05a8c6d43418ab9
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2025-01-13 18:48:33 +0000

    fix(minor fix): minor change

--- LI4/LI4.Common/IMineBuildsLN.cs
+++ LI4/LI4.Common/IMineBuildsLN.cs
@@ -4,6 +4,7 @@ namespace LI4.Common;
 public interface IMineBuildsLN {
 
     public bool authenticate(string username, string password);
+    
     public bool updateUtilizador(string email, string username,string password);
 
     public Task<Utilizador> getUser(string email);
