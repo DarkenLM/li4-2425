c3ec68176cc77ee0599205bf336849b55295a10e
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2025-01-13 18:44:39 +0000

    feat(getuser): adds getUser to IMinebuilds

--- LI4/LI4.Common/IMineBuildsLN.cs
+++ LI4/LI4.Common/IMineBuildsLN.cs
@@ -1,6 +1,10 @@
-﻿namespace LI4.Common;
+﻿using LI4.Dados;
+
+namespace LI4.Common;
 public interface IMineBuildsLN {
 
     public bool authenticate(string username, string password);
     public bool updateUtilizador(string email, string username,string password);
+
+    public Task<Utilizador> getUser(string email);
 }
