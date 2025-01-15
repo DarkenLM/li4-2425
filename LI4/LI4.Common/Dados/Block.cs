e6a65d40553bb8e94a128dfad4e5d27c5abd446e
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2025-01-15 21:55:48 +0000

    fix(missing changes): adds some missing changes

--- LI4/LI4.Common/Dados/Block.cs
+++ LI4/LI4.Common/Dados/Block.cs
@@ -1,9 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace LI4.Common.Dados {
+    public enum BlockRarity {
+        COMMON,
+        RARE,
+        EPIC
+    }
 
-namespace LI4.Common.Dados;
-internal class Block {
+    public class Block {
+        private int id { get; set; }
+        private string name { get; set; }
+        private BlockRarity rarity { get; set; }
+        private int timeToAcquire { get; set;  }
+        private int orderID { get; set;  }
+
+        public Block(int id, string name, BlockRarity rarity, int timeToAcquire, int orderID) {
+            this.id = id;
+            this.name = name;
+            this.rarity = rarity;
+            this.timeToAcquire = timeToAcquire;
+            this.orderID = orderID;
+        }
+    }
 }
--- LI4/LI4.Common/Dados/BlockDAO.cs
+++ LI4/LI4.Common/Dados/BlockDAO.cs
@@ -1,9 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Dapper;
+using LI4.Common.Exceptions.UserExceptions;
+using LI4.Dados;
+using Microsoft.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LI4.Common.Dados;
-internal class BlockDAO {
+
+/// <summary>
+/// Provides an access point to the table LI4.Bloco on the database and provides CRUD operations within that table.
+/// 
+/// All methods need to be run in an asynchronous environment.
+/// 
+/// <example>
+/// Example:
+/// <code>
+/// Block block = await BlockDAO.GetByIdAsync(1); // Block{id=1, username=abc, email=a@b.c, timeToAcquire=123, orderID=1}
+/// </code>
+/// </example>
+/// 
+/// See LI4.Client#Pages.Users for more examples.
+/// </summary>
+public class BlockDAO {
+    private readonly string connectionString;
+
+    public BlockDAO(string conStr) {
+        this.connectionString = conStr;
+    }
+
+    private SqlConnection getConnection() => new SqlConnection(this.connectionString);
+
+    public async Task<Block> getByIdAsync(int id) {
+        
+        // Problema, o bloco tem mais informação do que a retirada por aqui (tenho que aceder a propriedadeBloco)
+        using var connection = getConnection();
+        const string query = "SELECT * FROM Utilizador WHERE id = @Id";
+        return await connection.QueryFirstOrDefaultAsync<Block>(query, new { id = id });
+    }
 }
