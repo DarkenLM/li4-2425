d753d0d1375dccf3764168c8b89dac492a828a76
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2025-01-16 23:11:27 +0000

    feat(construction): adds constructionDAO

---
+++ LI4/LI4/Controllers/DAO/ConstructionDAO.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using LI4.Common.Exceptions.UserExceptions;
+using LI4.Dados;
+using Microsoft.Data.SqlClient;
+
+namespace LI4.Controllers.DAO;
+
+public class ConstructionDAO {
+    private readonly string connectionString;
+
+    public ConstructionDAO(string conStr) {
+        this.connectionString = conStr;
+    }
+
+    private SqlConnection getConnection() => new SqlConnection(this.connectionString);
+
+    #region//---- CRUD OPERATIONS ----//
+
+    public async Task<bool> deleteConstructionInstanceAsync(int id) {
+        using var connection = getConnection();
+        const string query = "DELETE FROM Constructions WHERE id = @Id";
+        int rowsAffected = await connection.ExecuteAsync(query, new { Id = id });
+        return rowsAffected > 0;
+    }
+    #endregion//---- END ----/
+
+}
