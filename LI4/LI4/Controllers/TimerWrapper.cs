b37afef0c8a1d891fd9f0e801839953cd14200eb
Paulo Ferreira <pauloferreira.vnc@gmail.com>
2025-01-17 06:35:01 +0000

    docs(documentation): adds documentation to timerWrapper

--- LI4/LI4/Controllers/TimerWrapper.cs
+++ LI4/LI4/Controllers/TimerWrapper.cs
@@ -3,7 +3,11 @@ using System.Threading;
 
 namespace LI4.Controllers;
 
-public class TimerWrapper : IDisposable {
+/**
+ * If necessary to edit this class more information here
+ * https://learn.microsoft.com/en-us/dotnet/api/system.threading.timer?view=net-9.0
+ */
+public class TimerWrapper {
     private Timer _timer;
     private Action _callback;
     private int _delayMilliseconds;
@@ -27,15 +31,22 @@ public class TimerWrapper : IDisposable {
         _timer = new Timer(timerCallback, null, Timeout.Infinite, Timeout.Infinite);
     }
 
+    /**
+     * for know, this function is not to be used
+     * We can if necessary use an object as a parameter an use variables and methods from that object
+     */
     public void timerCallback(object state) {
         _callback.Invoke();
 
         if (!_isLooping) {
-            Dispose();
+            dispose();
         }
     }
 
-    public void Start() {
+    /**
+     * This function starts the timer with the values used to create the object
+     */
+    public void start() {
         if (_isLooping) {
             _timer.Change(_delayMilliseconds, _delayMilliseconds);
         } else {
@@ -43,11 +54,17 @@ public class TimerWrapper : IDisposable {
         }
     }
 
-    public void Stop() {
+    /**
+     * This function stops the timer, it is usefull if the timer has a loop or if we want to stop mid run.
+     */
+    public void stop() {
         _timer?.Change(Timeout.Infinite, Timeout.Infinite);
     }
 
-    public void Dispose() {
+    /**
+     * This function disposes the timer (Releases all resources used by the current instance of Timer.)
+     */
+    public void dispose() {
         _timer?.Dispose();
         _timer = null;
     }
