2ee64a58aa5a2af57f909ffd2daa9e92230efeb6
alexaraujosa <alexaraujosa@hotmail.com>
2025-01-17 17:42:03 +0000

    fix(block): fixed class missing variable

--- LI4/LI4.Common/Dados/Block.cs
+++ LI4/LI4.Common/Dados/Block.cs
@@ -7,14 +7,16 @@
 
     public class Block {
         public int id { get; set; }
+        public int quantity { get; set; }
         public string name { get; set; }
         public BlockRarity rarity { get; set; }
         public int timeToAcquire { get; set;  }
 
         public Block() { }
 
-        public Block(int id, string name, BlockRarity rarity, int timeToAcquire) {
+        public Block(int id, int quantity, string name, BlockRarity rarity, int timeToAcquire) {
             this.id = id;
+            this.quantity = quantity;
             this.name = name;
             this.rarity = rarity;
             this.timeToAcquire = timeToAcquire;
